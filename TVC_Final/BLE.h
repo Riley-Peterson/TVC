4ffd62aa3a01d17f074fb1663123f4e48a0b5f92
Riley-Peterson <rpetersontx@gmail.com>
2024-07-22 14:13:47 -0500

    Advertise BLE on disconnect

--- TVC_Final/BLE.h
+++ TVC_Final/BLE.h
@@ -25,7 +25,7 @@ class MyServerCallbacks: public BLEServerCallbacks {
     };
     void onDisconnect(BLEServer* pServer) {
       deviceConnected = false;
-      
+      pServer->getAdvertising()->start();
     }
 };
 
